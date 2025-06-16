3d07acf91c1c34cf54e9c87e947c23e2e9d5bbd7
José Roberto <jrsouza47@gmail.com>
2025-06-16 08:50:03 -0300

    Correção da craição da tabela usuários

--- migrations/20250613201451_criar_tabela_usuarios.js
+++ migrations/20250613201451_criar_tabela_usuarios.js
@@ -3,6 +3,7 @@ exports.up = function(knex) {
     table.increments('id').primary();
     table.string('nome').notNullable();
     table.string('email').notNullable().unique();
+    table.string('senha').notNullable();
   });
 };
 
